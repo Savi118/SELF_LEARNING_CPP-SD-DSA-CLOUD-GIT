727b2cc4cf4de4de3acaf7ccd9967cdaafdc6d2b
Savi118 <sakshamviraj@amityonline.com>
2026-07-21 03:31:43 +0530

    Solution for Q-02
    Solution for Q-02

---
+++ Day-01/05-Practice Questions/Q-02.cpp
@@ -0,0 +1,14 @@
+// 2. Declare an int, a float, and a bool — print all three on one line using chained <<
+
+#include <iostream>
+using namespace std;
+
+int main() {
+        int marks = 489;
+        float percentage = 97.8f;
+        bool isPass = true;
+
+        cout << "This student got " << marks << " and the percentage is " << percentage << "% and is pass - " << isPass << "." << endl;
+
+        return 0;
+}
