bac77f0ed3326b1462e3de9acdaa5c7453a7e604
Savi118 <sakshamviraj@amityonline.com>
2026-07-21 00:56:28 +0530

    02-Anatomy
    Write and learn the basic syntax of c++

---
+++ Day-01/02-Anatomy/Explain.cpp
@@ -0,0 +1,7 @@
+#include <iostream>   // (a) imports input/output tools
+using namespace std;  // (b) lets you write cout instead of std::cout
+
+int main() {           // (c) every program starts execution here
+    cout << "text";    // (d) cout = "console output", << sends data to it
+    return 0;           // (e) tells the OS the program finished successfully
+}
