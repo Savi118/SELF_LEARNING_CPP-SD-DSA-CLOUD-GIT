de7627b2e6c388280db8e27517b63991c4ca7906
Savi118 <sakshamviraj@amityonline.com>
2026-07-21 03:31:28 +0530

    Solution for Q-01
    Solution for Q-01

---
+++ Day-01/05-Practice Questions/Q-01.cpp
@@ -0,0 +1,16 @@
+// 1. Print your name, age, and city using 3 separate cout statements
+
+#include <iostream>
+using namespace std;
+
+int main() {
+        string name = "Savi";
+        string city = "Noida";
+        int age = 22;
+
+        cout << "Name : " << name << endl;
+        cout << "Age : " << age << endl;
+        cout << "City : " << city << endl;
+
+        return 0;
+}
