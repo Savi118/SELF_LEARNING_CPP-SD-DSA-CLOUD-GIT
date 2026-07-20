93b8d55c7d208a508b0fda6597229f6e5955aa4a
Savi118 <sakshamviraj@amityonline.com>
2026-07-21 01:34:30 +0530

    03-Variables and Data Types
    I learn how to initiate variables and how many data types are in c++ and how to print it in terminal or output.

---
+++ Day-01/03-Variables & Data Types/Variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+        int age = 21;
+        float height = 5.95f;
+        char grade = 'A';
+        bool isStudent = true;
+        string name = "Saksham";
+
+        cout << "Name : " << name << endl;
+        cout << "Age : " << age << endl;
+        cout << "Height : " << height << endl;
+        cout << "Grade : " << grade << endl;
+        cout << "Is Student : " << isStudent << endl;
+
+        return 0;
+}
