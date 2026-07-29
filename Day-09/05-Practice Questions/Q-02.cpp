413035616c94170a52077b378b453a66b66c8749
Savi118 <sakshamviraj@amityonline.com>
2026-07-29 20:25:53 +0530

    Solution for Q-02

---
+++ Day-09/05-Practice Questions/Q-02.cpp
@@ -0,0 +1,43 @@
+// 2. Implement selectionSort from scratch, test on the same array
+
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+        cout << "arr[" << size << "] : {";
+        for (int i = 0; i < size; i++)
+        {
+                cout << arr[i] << ",";
+        }
+        cout << "}" << endl;
+}
+
+void selectionSort(int arr[], int size)
+{
+        for (int i = 0; i < size - 1; i++)
+        {
+                int midIndex = i;
+                for (int j = i + 1; j < size; j++)
+                {
+                        if (arr[j] < arr[midIndex])
+                        {
+                                midIndex = j;
+                        }
+                }
+                int temp = arr[i];
+                arr[i] = arr[midIndex];
+                arr[midIndex] = temp;
+        }
+
+        printArray(arr, size);
+}
+
+int main()
+{
+        int arr[] = {5, 2, 4, 1};
+        int size = sizeof(arr) / sizeof(arr[0]);
+
+        selectionSort(arr, size);
+        return 0;
+}
