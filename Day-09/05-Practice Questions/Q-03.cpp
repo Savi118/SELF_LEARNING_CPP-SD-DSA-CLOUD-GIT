959dd01168c3668d9648003c04efed2d568d740f
Savi118 <sakshamviraj@amityonline.com>
2026-07-29 20:27:54 +0530

    Solution for Q-03

---
+++ Day-09/05-Practice Questions/Q-03.cpp
@@ -0,0 +1,42 @@
+// 3. Implement insertionSort from scratch, test on the same array
+
+// 2. Implement selectionSort from scratch, test on the same array
+
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+        cout << "arr[" << size << "] : {";
+        for (int i = 0; i < size; i++)
+        {
+                cout << arr[i] << ",";
+        }
+        cout << "}" << endl;
+}
+
+void insertionSort(int arr[], int size)
+{
+        for (int i = 0; i < size; i++)
+        {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                        arr[j + 1] = arr[j];
+                        j--;
+                }
+                arr[j + 1] = key;
+        }
+
+        printArray(arr, size);
+}
+
+int main()
+{
+        int arr[] = {5, 2, 4, 1};
+        int size = sizeof(arr) / sizeof(arr[0]);
+
+        insertionSort(arr, size);
+        return 0;
+}
